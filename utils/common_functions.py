e758b206b913381c285a7717a3968968788f6e5a
HakimOwais <owaisibnmushtaq@gmail.com>
2025-03-20 22:59:17 +0530

    common function and bucket storage

---
+++ utils/common_functions.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+
+
+from source.custom_exception import CustomException
+from source.custom_logger import get_logger
+
+logger = get_logger(__name__)
+
+def read_yaml(file_path):
+    try:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"File not found")
+        
+        with open(file_path, "r") as yaml_file:
+            config = yaml.safe_load(yaml_file)
+            logger.info("Successfully read the YAML file")
+            return config
+        
+    except Exception as e:
+        logger.error("Error ")
+        raise CustomException("Failed to read ")
+    
